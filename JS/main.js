b6d47a8effc07542bee794d26747a3e96c819d05
Macmer88 <84930872+Macmer88@users.noreply.github.com>
2023-07-03 07:37:56 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -13,13 +13,7 @@ document.getElementById(
     <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav me-auto mt-2 mt-lg-0">
            
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
-                <div class="dropdown-menu" aria-labelledby="dropdownId">
-                    <a class="dropdown-item" href="productos.html">Productos</a>
-                    <a class="dropdown-item" href="#">Action 2</a>
-                </div>
-            </li>
+        
         </ul>
         <form class="d-flex my-2 my-lg-0">
             <input class="form-control me-sm-2" type="text" placeholder="Search">
