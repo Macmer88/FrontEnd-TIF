85a99a49ea573687ef7ec12790a3174dcee0f204
Macmer88 <84930872+Macmer88@users.noreply.github.com>
2023-07-03 07:35:08 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -12,12 +12,7 @@ document.getElementById(
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-                <a class="nav-link active" href="index.html" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
+           
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
                 <div class="dropdown-menu" aria-labelledby="dropdownId">
