48572e507f3c737b2ef9160ba8e8cc3a017b3e70
Macmer88 <84930872+Macmer88@users.noreply.github.com>
2023-07-13 11:24:02 -0300

    Update producto-edit.js
    actualizada url BD

--- js/producto-edit.js
+++ js/producto-edit.js
@@ -11,7 +11,7 @@ createApp({
       imagen: "",
       stock: 0,
       precio: 0,
-      url: "http://localhost:5000/productos/" + id,
+      url: "https://maximilianomerele88.pythonanywhere.com/productos/" + id,
     };
   },
   methods: {
